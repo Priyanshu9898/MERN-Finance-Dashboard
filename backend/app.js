2dd589305eedb0ba9c44b1485fb1dfe79771baa8
Priyanshu9898 <priyanshumalaviya17@gmail.com>
2023-04-07 20:33:18 +0530

    Database connected successfully

--- backend/app.js
+++ backend/app.js
@@ -19,9 +19,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 const port = process.env.PORT || 4000;
-
 
 app.listen(port, (req, res) => {
     console.log(`App listening on http://localhost:${port}`)
