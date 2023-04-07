f8cdbbc19a4693a9ea16c64b4489eb9165f24b68
Priyanshu9898 <priyanshumalaviya17@gmail.com>
2023-04-07 20:34:16 +0530

    Database connected successfullly

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = async (req, res) => {
     const db = process.env.MONGO_URL;
 
-    const {conn} = await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true});
+    const conn = await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true});
 
-    console.log(`MongoDB Connected Successfully, ${conn}`);
+    console.log(`MongoDB Connected Successfully`);
 
 }
